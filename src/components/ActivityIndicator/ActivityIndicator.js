9b2f30bf53e00d7c1f91ad97fc7a33635dbd22b6
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-04-04 01:45:26 -0400

    added activity spinner while images load

--- src/components/ActivityIndicator/ActivityIndicator.js
+++ src/components/ActivityIndicator/ActivityIndicator.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './ActivityIndicator.css'
 
-const ActivityIndicator = props => {
+const ActivityIndicator = ({size}) => {
+  const smallClass = size === 'small' ? ' ActivityIndicator__spinner--small' : '' 
   return (
     <div className="ActivityIndicator">
-      Loading...
+      <div className={'ActivityIndicator__spinner' + smallClass}></div>
     </div>
   )
 }
--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,17 +1,44 @@
 import React from 'react'
 import './Poster.css'
 
-const Poster = ({size, config, path}) => {
-  if (!config) {
-    return <div className="Poster"></div>
+import ActivityIndicator from '../ActivityIndicator/ActivityIndicator'
+
+class Poster extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      imageLoaded: false
+    }
   }
-  const {base_url, poster_sizes} = config
-  const sizeString = size === 'large' ? poster_sizes[poster_sizes.length - 2] : poster_sizes[0]
-  const url = base_url + sizeString + path
-  
-  return (
-    <img className="Poster" src={url} alt="Movie Poster" />
-  )
+
+  handleImgLoad = (e) => {
+    this.setState({imageLoaded: true})
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.path !== prevProps.path) {
+      this.setState({imageLoaded: false})
+    }
+  }
+
+  render() {
+    const {config, size, path} = this.props
+    if (!config) {
+      return <div className="Poster"></div>
+    }
+    const {base_url, poster_sizes} = config
+    const sizeString = size === 'large' ? poster_sizes[poster_sizes.length - 2] : poster_sizes[0]
+    const url = base_url + sizeString + path
+    const waitingClass = this.state.imageLoaded ? '' : ' Poster--waiting'
+    return (
+      <div className="Poster__wrapper">
+        <img className={'Poster' + waitingClass} src={url} alt="Movie Poster" onLoad={this.handleImgLoad} />
+        {
+          !this.state.imageLoaded && <ActivityIndicator size={size} />
+        }
+      </div>
+    )
+  }
 }
 
 export default Poster
